23acca8413a6e7746180c5035aec6781ddc175c3
Dummy <info@jobstogo.io>
2019-03-03 12:40:01 +0100

    removed unused exception.

--- src/FluffySpoon.Automation.Web/Exceptions/NavigationUnderwayException.cs
+++
@@ -1,9 +0,0 @@
-﻿using FluffySpoon.Automation.Web.Fluent.Expect;
-using System;
-
-namespace FluffySpoon.Automation.Web.Exceptions
-{
-	public class NavigationUnderwayException : AutomationException
-	{
-	}
-}
